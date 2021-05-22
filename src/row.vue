1b2ae69b28727df911b0b0e8f506fce21a5edd50
Lionyu <779625444@qq.com>
2021-05-22 17:45:25 +0800

    添加 row 的 align 的 left right center 属性

--- src/row.vue
+++ src/row.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="row" :style="{
-    marginLeft:-gutter/2+'px',
-    marginRight:-gutter/2+'px'}">
+  <div class="row" :style="rowStyle" :class="rowClass"> 
     <slot></slot>
   </div>
 </template>
@@ -12,13 +10,28 @@ export default {
   props:{
     gutter:{
       type:[Number,String]
+    },
+    align:{
+      type:String,
+      validator(value){
+        return ['left','right','center'].includes(value)
+      }
     }
   },
-  created(){
-    console.log('row created')
+  computed:{
+    rowStyle(){
+      let {gutter} = this
+      return {
+        marginLeft:-gutter/2+'px',
+        marginRight:-gutter/2+'px'
+        }
+      },
+      rowClass(){
+        let {align} = this
+        return [align && `align-${align}`]
+      }
   },
   mounted(){
-    console.log('row mounted')
     this.$children.forEach((vm)=>{
       vm.gutter = this.gutter
     })
@@ -29,5 +42,14 @@ export default {
 <style lang="scss" scoped>
   .row{
     display: flex;
+    &.align-left{
+      justify-content: flex-start;
+    }
+    &.align-right{
+      justify-content: flex-end;
+    }
+    &.align-center{
+      justify-content: center;
+    }
   }
 </style>
