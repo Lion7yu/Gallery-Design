a4a3bbd8e1a97b87515b2b2755825dc8121dd4d9
Lionyu <779625444@qq.com>
2021-05-23 01:10:02 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 <script>
+  let validator = (value)=>{
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key=>{
+    if(!['span','offset'].includes(key)){
+      valid = false
+      }
+    })
+    return valid
+  }
 export default {
   name:'LCol',
   props:{
@@ -13,6 +23,26 @@ export default {
     offset:{
       type:[Number,String]
     },
+    xs:{
+      type:Object,
+      validator,
+    },
+    ms:{
+      type:Object,
+      validator,
+    },
+    md:{
+      type:Object,
+      validator,
+    },
+    lg:{
+      type:Object,
+      validator,
+      },
+    xl:{
+      type:Object,
+      validator,
+    },
   },
   data(){
     return{
@@ -21,16 +51,22 @@ export default {
   },
   computed:{
     colClass(){
-      let {span,offset} = this
+      let {span,offset,xs,ms,md,lg,xl} = this
+      let phoneClass = []
       return [
         span && `col-${span}`,
-        offset && `offset-${offset}`
+        offset && `offset-${offset}`,
+        ...(xs && [`col-xs-${xs.span}`]),
+        ...(ms && [`col-ms-${ms.span}`]),
+        ...(md && [`col-md-${md.span}`]),
+        ...(lg && [`col-lg-${lg.span}`]),
+        ...(xl && [`col-xl-${xl.span}`]),
         ]
     },
     colStyle(){
       return{
-      paddingLeft:this.gutter / 2 + 'px',
-      paddingRight:this.gutter / 2 + 'px',
+        paddingLeft:this.gutter / 2 + 'px',
+        paddingRight:this.gutter / 2 + 'px',
       }
     }
   }
@@ -48,6 +84,76 @@ export default {
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n}{ //插值语法
         margin-left: ($n/24)*100%;
+      }
+    }
+    @media(max-width:576px){
+    $class-prefix:col-xs-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix:offset-xs-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media(min-width:576px) and (max-width:768px){
+    $class-prefix:col-sm-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix:offset-sm-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media(min-width:768px) and (max-width:992px){
+    $class-prefix:col-md-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix:offset-md-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media(min-width:992px) and (max-width:1200px){
+    $class-prefix:col-lg-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix:offset-lg-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media(min-width:1201px){
+    $class-prefix:col-xl-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix:offset-xl-; //声明一个class前缀
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{ //插值语法
+        margin-left: ($n/24)*100%;
+        }
       }
     }
   }
